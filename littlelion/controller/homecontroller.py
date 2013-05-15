f796573bf59d9e5a9bece94cef9e33bc7fdfb366
pleasewander <stiltz.ryan@gmail.com>
2013-05-14 17:07:50 -0700

    trying out render instead of render_to_response

--- littlelion/controller/homecontroller.py
+++ littlelion/controller/homecontroller.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 def homehandler(request):
-    return render_to_response('home.html')
+    return render(request,'home.html')
 
 
 urlpatterns = patterns('littlelion.controller.homecontroller',
