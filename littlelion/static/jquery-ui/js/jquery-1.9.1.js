04538f72b60789f8c44f2db58bf6d5c6732411c6
pleasewander <stiltz.ryan@gmail.com>
2013-05-18 16:19:37 -0700

    implemented includes folders for the header, nav, and bottom. Completed initial look of top navigation

--- littlelion/static/jquery-ui/js/jquery-1.9.1.js
+++ littlelion/static/jquery-ui/js/jquery-1.9.1.js
@@ -7037,7 +7037,7 @@ function augmentWidthOrHeight( elem, name, extra, isBorderBox, styles ) {
 		}
 
 		if ( isBorderBox ) {
-			// border-box includes padding, so remove it if we want content
+			// border-box include padding, so remove it if we want content
 			if ( extra === "content" ) {
 				val -= jQuery.css( elem, "padding" + cssExpand[ i ], true, styles );
 			}
--- littlelion/wsgi.py
+++ littlelion/wsgi.py
@@ -22,7 +22,7 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "littlelion.settings")
 
 # This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
+# file. This include Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
